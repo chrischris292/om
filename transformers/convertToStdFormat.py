cc67de937916d6769772ffde3030e9745cb486f3
saurabhjha <saurabhjha@ac49.(none)>
2015-03-02 05:33:07 -0600

    updated convertToStdFormat.py to account for old node mapping transformation

--- transformers/convertToStdFormat.py
+++ transformers/convertToStdFormat.py
@@ -7,19 +7,37 @@ Converts to the format given by Lelio. In future the whole workflow will be modi
 
 import os,sys
 
-global BASE_DIR 
-def changeFormatMce(in_file,map_file):
-	BASE_DIR = '~/om'
-	# node id ---> node type
-	node_map = {}
+node_map = {}
+nid_map = {}
+node_map_old = {}
+nid_map_old = {}
+
+def node_mapper(new_map, old_map):
+ 	# node id ---> node type
+	global	node_map
+	global node_map_old 
 	# node id ----> nid
-	nid_map = {}
-	mapF = open(map_file, 'r')
+	global nid_map
+	global nid_map_old
+	mapF = open(new_map, 'r')
 	for map in mapF:
 		node_map[map.split()[0]] = map.split()[1]
 		nid_map[map.split()[0]] = map.split()[2]
 	mapF.close()
+	#load old mapping
+	mapF = open(old_map, 'r')
+	for map in mapF:
+		node_map_old[map.split()[0]] = map.split()[1]
+		nid_map_old[map.split()[0]] = map.split()[2]
+	mapF.close()
 	
+def changeFormatMce(new_map, old_map, in_file):
+
+	global node_map
+	global node_map_old
+	global nid_map
+	global nid_map_old
+	node_mapper(new_map, old_map)
 	msgs = open(in_file, 'r')
 	op_out = open(in_file+"_formatted", 'w')
 	for msg in msgs:
@@ -36,8 +54,18 @@ def changeFormatMce(in_file,map_file):
 		#print(cx_cy, chasis)
 		slot = ((node_id.split('c')[2]).split('s')[1]).split('n')[0]
 		node = ((node_id.split('c')[2]).split('s')[1]).split('n')[1]
-		node_type =node_map[node_id] #tag using file
-		nid = nid_map[node_id]
+		try:
+			if int(timestamp) > 1373886000:
+				#use new mapping
+				node_type =node_map[node_id] #tag using file
+				nid = nid_map[node_id]
+			else:
+				node_type = node_map_old[node_id]
+				nid = nid_map_old[node_id]
+		except:
+			node_type = node_id
+			nid = 0
+	
 		log_msg = ' '.join((" ".join(columns[9:])).split())
 		#format
 		filtered_msg = timestamp + "\t" + date + " " + time + "\t" + node_id + "\t" + node_type + "\t" + cx_cy + "\t"  + chasis + "\t" + slot + "\t" + node + "\t" + nid + "\t" + log_msg
@@ -46,10 +74,10 @@ def changeFormatMce(in_file,map_file):
 	msgs.close()
 	op_out.close()
 def main():
-	if len(sys.argv) < 3:
-		print "Usage: ./convert <formatted log file > <node mapping file>"
+	if len(sys.argv) < 4:
+		print "Usage: ./convert < new node mapping file> <old node mapping file> <one or more formatted logs>"
 		return
-	changeFormatMce(sys.argv[1],sys.argv[2])
+	changeFormatMce(sys.argv[1],sys.argv[2], sys.argv[3])
 	return
 
 if __name__ == "__main__":
